94729cfcbe31c56a067d363614a39a3268cac6b0
Georgemaine <georgemaine.lourens@gmail.com>
2025-04-29 15:14:34 +0200

    Polish ui

--- components/HomeContent/LandingForm.tsx
+++ components/HomeContent/LandingForm.tsx
@@ -4,6 +4,7 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { a, SpringValue } from "@react-spring/web";
 import { getGalleryItemPath, isValidUrl } from "@/lib/utils";
+import { useAppDispatch } from "@/lib/store";
 
 interface LandingFormProps {
   spring: SpringValue;
@@ -11,33 +12,34 @@ interface LandingFormProps {
 
 export function LandingForm(props: LandingFormProps) {
   const { spring } = props;
+  const dispatch = useAppDispatch();
+
   function generatePage(formData: FormData) {
-    const query = formData.get("query");
-    console.log(formData);
-    if (!query) return;
-    const validUrl = isValidUrl(query as string);
+    const website = formData.get("query");
+    if (!website) return;
+    const validUrl = isValidUrl(website as string);
 
     if (validUrl) {
       // window.location.href = query as string; // handle trim logic
-      window.location.href = getGalleryItemPath(query as string);
+      window.location.href = getGalleryItemPath(website as string);
     } else {
-      window.location.href = getGalleryItemPath(query as string);
+      window.location.href = getGalleryItemPath(website as string);
     }
   }
   return (
     <a.div
-      className="flex items-center w-[600px] p-[1px]"
+      className="flex items-center w-full p-[1px]"
       style={{
         transform: spring.to([0, 1], ["scale(1)", "scale(1.019)"]),
       }}
     >
-      <div className="relative z-10">
+      <div className="relative z-10 w-full">
         <form className="flex items-center" action={generatePage}>
           <Input
             type="text"
             name="query"
             placeholder="Enter company name"
-            className="bg-white h-16 w-[600px] border-px border-(--theme-color-fillQuaternary) !text-lg pl-5 pr-5 shadow-[0_11px_34px_rgba(0,0,0,0.07)] focus:ring-2 focus:ring-[--theme-color-appTint] rounded-[14px]"
+            className="bg-white h-16 w-[full border-px border-(--theme-color-fillQuaternary) !text-lg pl-5 pr-5 shadow-[0_11px_34px_rgba(0,0,0,0.07)] focus:ring-2 focus:ring-[--theme-color-appTint] rounded-[14px]"
           />
           <div className="absolute right-2">
             <Button
--- components/HomeContent/index.tsx
+++ components/HomeContent/index.tsx
@@ -11,16 +11,16 @@ const HomeContent = () => {
     <>
       <NavigationBar preview={false} />
       <main className="w-full h-full flex min-h-screen flex-col relative">
-        <div className="relative flex flex-col items-center justify-center mt-52 z-10">
+        <div className="relative flex flex-col items-center justify-center mt-52 z-10 max-w-[600px] px-5 mx-auto w-[calc(100%-40px)]">
           <Logo />
-          <h1 className="text-8xl font-bold leading-20 text-center tracking-tighter mt-5">
+          <h1 className="text-6xl leading-16 m:text-8xl font-bold m:leading-20 text-center tracking-tighter mt-5">
             Discovery
           </h1>
           <p className="text-xl font-medium leading-6 text-center text-neutral-500 max-w-sm mt-6">
             Use AI to uncover domain opportunities and analyze top pages with
             SEO insights
           </p>
-          <CursorTarget enabled={true} className={"mt-10"}>
+          <CursorTarget enabled={true} className={"mt-10 w-full"}>
             {(spring) => <LandingForm spring={spring} />}
           </CursorTarget>
           <p className="text-xs uppercase text-neutral-400 tracking-widest mt-10">
